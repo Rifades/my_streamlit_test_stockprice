cd77af59db86944688dd4ff3f5e74fbcea283a47
Mir Rejaul Islam <114472389+Rifades@users.noreply.github.com>
2026-02-06 10:03:09 +0000

    3rd Correction

--- streamlit_stock.py
+++ streamlit_stock.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 
+
+
+tickers = st.sidebar.text_input("Enter Stock Name")
 b = st.sidebar.selectbox("Select a Timeline", ["7d", "15d", "1mo", "3mo", "6mo", "1y"])
 
+tickers = tickers.strip().split(",")
 
-tickers = ["AAPL", "MSFT", "GOOGL", "TSLA"]
+#tickers = ["AAPL", "MSFT", "GOOGL", "TSLA"]
 
 
 data = yf.download(tickers, period=b)["Close"]
